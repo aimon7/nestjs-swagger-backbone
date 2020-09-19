90f90783e074087e58c4d9081cbdedd912c78da0
aimon7 <air.ioannis@gmail.com>
2020-09-19 18:00:04 +0300

    Added ormconfig.json to replace typeorm.config.ts
    Added ormconfig.json to replace typeorm.config.ts, beacuse it was confusing the migrations.
    Also added in package.json some new scripts for migrations.
    And updated README.md

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from '../config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot(),
     AuthModule,
     UserModule],
   controllers: [],
